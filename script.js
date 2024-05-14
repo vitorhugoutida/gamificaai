c39b92c4336cbf9c43ede7a3a4f9299d237b198a
vitorhugoutida <vitorhugoutida@gmail.com>
2024-05-14 17:24:16 -0300

    feat: quase acabando o carousel no banner

--- script.js
+++ script.js
@@ -53,3 +53,70 @@ window.onresize = () => {
 //     iconeX.style.display = "inline"
 //     iconeBarras.style.display = "none"
 // }
+
+// funcao carrosel
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+// let ultimoItem = slide[slides.length - 1]
+
+let slideAtual = 0
+
+let numeroSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+    // Remove slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual < (numeroSlides - 1)) {
+        slideAtual++
+        console.log("slideAtual++");
+
+    } else {
+        slideAtual = 0
+    }
+
+    // if(slideAtual == slides.indexOf(ultimoItem) ) {
+
+    //     slideAtual++
+
+    // } else {
+    //     slideAtual = 0
+    // }
+
+
+
+// Muda a posicao da lista de slides, para mostrar o slideAtual
+
+// Renderiza o slideAtual
+banner.classList.add(slides[slideAtual])
+}
+
+
+const mostrarSlideAnterior = () => {
+    // Remove slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        slideAtual--
+
+    } else {
+        slideAtual = numeroSlides - 1
+    }
+
+    // Muda a posicao da lista de slides, para mostrar o slideAtual
+
+    // Renderiza o slideAtual
+    banner.classList.add(slides[slideAtual])
+
+
+}
+
